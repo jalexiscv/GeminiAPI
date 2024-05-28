f9c9fc7d72a5e1f4eb7623409a38f7a3ee7f98b1
eniompw <71254710+eniompw@users.noreply.github.com>
2024-05-28 15:27:25 +0000

    loading bar

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, request
 import google.generativeai as genai
 import os
 
@@ -6,28 +6,14 @@ app = Flask(__name__)
 app.secret_key = os.urandom(16)
 genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 model = genai.GenerativeModel('gemini-1.5-flash')
-chunks = []
+response = ""
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', output="")
 
-@app.route("/query")
+@app.route('/query')
 def query():
     q = request.args.get('q')
-    response = model.generate_content(q, stream=True)
-    for chunk in response:
-        chunks.append(chunk.text)
-    return render_template('index.html')
-
-@app.route('/stream')
-def stream():
-    if 'index' in session:
-        if  int(session['index']) < len(chunks) - 1:
-            session['index'] += 1
-            return str(chunks[session['index']])
-        else:
-            return "END"
-    else:
-        session['index'] = -1
-        return ""
+    response = model.generate_content(q)
+    return render_template('index.html', output=response.text)
