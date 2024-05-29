36fcf291b582d2f390db76a9d4e2f6306838760c
eniompw <71254710+eniompw@users.noreply.github.com>
2024-05-29 08:23:37 +0000

    simple example

--- dev/python.py
+++ dev/python.py
@@ -4,7 +4,5 @@ import os
 genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 model = genai.GenerativeModel('gemini-1.5-flash')
 
-response = model.generate_content("What is the meaning of life?", stream=True)
-
-for chunk in response:
-  print(chunk.text, end="")
+response = model.generate_content("What is the meaning of life?")
+print(response.text)
